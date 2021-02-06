1d0b7e9faa2f4334a43eb879cfcae39b7a0aa4b8
SCHILD CLARA <clara.schild@etu.unistra.fr>
2021-02-06 21:06:03 +0100

    change Model name

--- example-app/app/Models/PotiAnimal.php
+++ example-app/app/Models/PotiAnimal.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PotiAnimaux extends Model
+class PotiAnimal extends Model
 {
     use HasFactory;
 }
