eb43d41aee9f6802176baa8e0be4642de8ee9222
SCHILD CLARA <clara.schild@etu.unistra.fr>
2021-02-06 21:06:57 +0100

    change migration name

--- example-app/database/migrations/2021_02_06_194652_create_poti_animaux_table.php
+++ example-app/database/migrations/2021_02_06_194652_create_poti_animaux_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePotiAnimauxesTable extends Migration
+class CreatePotiAnimauxTable extends Migration
 {
     /**
      * Run the migrations.
