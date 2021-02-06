5091794bf009171506620e4671ee0ec7f8847d37
SCHILD CLARA <clara.schild@etu.unistra.fr>
2021-02-06 22:41:30 +0100

    PotiAnimal add type function

--- example-app/app/Models/PotiAnimal.php
+++ example-app/app/Models/PotiAnimal.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class PotiAnimal extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the type of the poti animal.
+     */
+    public function type()
+    {
+        return $this->belongsTo(PotiAnimauxType::class);
+    }
 }
